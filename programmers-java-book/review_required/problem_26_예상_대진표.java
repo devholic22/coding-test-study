f8646fa5b43bb41b71ca998008e92b11268528d4
devholic22 <hyunjoon.tech@gmail.com>
2024-06-26 17:46:10 +0900

    fail: 예상 대진표

---
+++ programmers-java-book/review_required/problem_26_예상_대진표.java
@@ -0,0 +1,15 @@
+package review_required;
+
+// 24.06.26 (수)
+public class problem_26_예상_대진표 {
+
+    public int solution(int n, int a, int b) {
+        int answer;
+
+        for (answer = 0; a != b; answer++) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+        return answer;
+    }
+}
