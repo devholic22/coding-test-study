32090e4650c075486ef1dc02b3887d23fca6c8f1
devholic22 <hyunjoon.tech@gmail.com>
2024-07-01 10:16:04 +0900

    review: 기능 개발

--- programmers-java-book/problem_16_기능_개발.java
+++ programmers-java-book/problem_16_기능_개발.java
@@ -1,44 +1,44 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 // 24.06.21 (금)
+// 24.07.01 (월) review
 public class problem_16_기능_개발 {
 
+    // 작업의 개수: 100개 이하
+    // 작업 진도 1 ~ 100 미만
+    // 작업 속도: 100 이하
+    /*
+    의사 코드
+        1. progresses, speeds를 입력받는다.
+        2. 큐를 선언한다. 큐가 적합한 이유는 "뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포"되기 때문이다.
+        3. 작업이 끝나기까지 남은 일수를 담은 ArrayList를 선언한다.
+        4. progress와 speed를 토대로 완료되기까지 얼마나 걸리는지 계산할 메서드를 작성한다.
+        5. 각 일수 당 끝난 작업의 갯수를 담아둘 변수를 선언한다.
+        6. 큐에서 맨 앞에 있는 작업을 빼오고, 4번 메서드를 통해 시간을 계산한다. 그리고 5번 변수를 +1 한다. (시간만 넣어둬도 된다.)
+        7. 새롭게 빼온 작업의 시간이 6번 작업보다 일찍 끝난다면 5번 변수를 +1 하고, 그게 아니라면 새롭게 변수를 선언하고 더한다.
+        8. ArrayList를 int[]로 변환하여 반환한다.
+    */
     public int[] solution(int[] progresses, int[] speeds) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        ArrayDeque<Integer> store = new ArrayDeque<>();
-
-        for (int i = 0; i < progresses.length; i++) {
-            int time = calculateDoneDay(progresses[i], speeds[i]); // ex: 7
-            map.put(time, 0); // 각각의 time들이 HashMap key로 등록되도록 한다.
-            store.addLast(time); // 각각의 time들이 스택에 들어가도록 한다.
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < speeds.length; i++) {
+            queue.addLast(calculate(progresses[i], speeds[i]));
         }
-
-        int max = 0;
-        for (int key : map.keySet()) { // 최대 일수를 구한다. (ex: 20)
-            max = Math.max(max, key);
-        }
-
-        for (int day = 0; day <= max; day++) { // 0일째부터 max 일 때 까지 돌면서 해당 일자에 제거할 수 있는지 진행한다.
-            int count = 0;
-            while (!store.isEmpty() && store.peekFirst() <= day) { // 이때 맨 처음의 값이 day보다 작거나 같을 때에만 진행한다. 또한, 반복문으로 돌린다.
-                // 예시로 [10, 5, 3, 15]인 경우 처음 day가 10에 걸리므로 10 <= 10, 5 <= 10, 3 <= 10이 모두 해당된다. 단 15는 10보다 크므로 별도로 된다.
-                count++;
-                store.pollFirst();
+        while (!queue.isEmpty()) {
+            int time = 1;
+            int done = queue.pollFirst();
+            while (!queue.isEmpty() && queue.peekFirst() <= done) {
+                queue.pollFirst();
+                time++;
             }
-            if (count != 0) { // 무의미한 0은 추가되지 않도록 한다.
-                result.add(count);
-            }
-            count = 0;
+            list.add(time);
         }
-        return result.stream()
-                .mapToInt(x -> x)
+        return list.stream()
+                .mapToInt(i -> i)
                 .toArray();
     }
-
-    private int calculateDoneDay(int progress, int speed) {
+    public int calculate(int progress, int speed) {
         return (100 - progress) % speed == 0 ? (100 - progress) / speed : (100 - progress) / speed + 1;
     }
 }
