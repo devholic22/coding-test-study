c41a1758b2e4d1be146fc470db02ab4db6fdf8a7
devholic22 <hyunjoon.tech@gmail.com>
2024-07-04 10:46:53 +0900

    review: 트리 순회

--- programmers-java-book/problem_25_트리_순회.java
+++ programmers-java-book/problem_25_트리_순회.java
@@ -1,42 +1,58 @@
 import java.util.Arrays;
 
 // 24.06.26 (수)
+// 24.07.04 (목) review
 public class problem_25_트리_순회 {
 
+    // 이진 트리를 표현한 리스트 nodes를 인자로 받습니다.
+    // 예를 들어서 nodes가 [1, 2, 3, 4, 5, 6, 7]이면 다음과 같은 트리를 표현한 것입니다. (그림)
+    // 해당 이진 트리에 대하여 전위 순회, 중위 순회, 후위 순회 결과를 반환하는 solution() 함수를 구현하세요.
+    // 입력 노드값의 개수는 1개 이상 1,000개 이하이다.
+    // 노드값은 정수형이며, 중복되지 않는다.
+    // 권장 시간 복잡도 O(N)
+    /*
+    의사 코드
+        1. nodes를 입력 받는다.
+        2. 세 가지 순회를 문자열로 저장할 배열을 선언한다.
+        3. 전위 순회 값을 2번 배열의 0번째 요소에 담는다. parent - left - right
+        4. 중위 순회 값을 2번 배열의 1번째 요소에 담는다. left - parent - right
+        5. 후위 순회 값을 2번 배열의 2번째 요소에 담는다. left - right - parent
+     시간 복잡도: O(N)
+     */
     public static void main(String[] args) {
-        System.out.println("solve(new int[] {1, 2, 3, 4, 5, 6, 7}) = " + Arrays.toString(solve(new int[] {1, 2, 3, 4, 5, 6, 7})));
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7}; // zero-index 고려해야 함
+        System.out.println("solution(numbers) = " + Arrays.toString(solution(numbers)));
     }
 
-    public static String[] solve(int[] nodes) {
-        // 전위 순회, 중위 순회, 후위 순회 반환
+    public static String[] solution(int[] nodes) {
         String[] answer = new String[3];
-        // 전위 순회: mid - left - right 반복
+        // 전위 순회: parent - left - right
         answer[0] = preOrder(nodes, 0).trim();
-        // 중위 순회: left - mid - right 반복
-        answer[1] = midOrder(nodes, 0).trim();
-        // 후위 순회: left - right - mid 반복
+        // 중위 순회: left - parent - right
+        answer[1] = inOrder(nodes, 0).trim();
+        // 후위 순회: left - right - parent
         answer[2] = postOrder(nodes, 0).trim();
         return answer;
     }
 
-    private static String preOrder(int[] nodes, int index) {
-        if (nodes.length <= index) {
+    public static String preOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
             return "";
         }
         return nodes[index] + " " + preOrder(nodes, index * 2 + 1) + preOrder(nodes, index * 2 + 2);
     }
 
-    private static String midOrder(int[] nodes, int index) {
-        if (nodes.length <= index) {
+    public static String inOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
             return "";
         }
-        return midOrder(nodes, index * 2 + 1)+ nodes[index] + " " + midOrder(nodes, index * 2 + 2);
+        return inOrder(nodes, index * 2 + 1) + " " + nodes[index] + inOrder(nodes, index * 2 + 2);
     }
 
-    private static String postOrder(int[] nodes, int index) {
-        if (nodes.length <= index) {
+    public static String postOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
             return "";
         }
-        return postOrder(nodes, index * 2 + 1) + postOrder(nodes, index * 2 + 2)+ nodes[index] + " ";
+        return postOrder(nodes, index * 2 + 1) + postOrder(nodes, index * 2 + 2) + " " + nodes[index];
     }
 }
